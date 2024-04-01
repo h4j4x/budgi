0def5b9478bee24c4aa9bf64b6082235b9c48b3c
Michel Atucha <miche.atucha@gmail.com>
2024-04-01 09:12:16 -0500

    spring redis cache entry ttl

--- spring/src/main/java/com/sp1ke/budgi/api/data/cache/RedisConfig.java
+++ spring/src/main/java/com/sp1ke/budgi/api/data/cache/RedisConfig.java
@@ -2,6 +2,7 @@ package com.sp1ke.budgi.api.data.cache;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -19,6 +20,7 @@ public class RedisConfig {
             .configure(SerializationFeature.INDENT_OUTPUT, false);
         var serializer = new GenericJackson2JsonRedisSerializer(mapper);
         return RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofDays(7))
             .disableCachingNullValues()
             .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
     }
